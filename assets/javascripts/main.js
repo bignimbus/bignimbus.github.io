07da5414bde62b4a605ffae70b3d7cf162993ca4
Jeff Auriemma <jdauriemma@gmail.com>
2016-06-28 21:45:32 +0000

    Site updated at 2016-06-28 21:45:32 UTC

---
+++ assets/javascripts/main.js
@@ -0,0 +1,34 @@
+(function () {
+  'use strict';
+
+  var $body = $(document.body);
+
+  function colorize () {
+    $('.page-content *:not(.social-media-list) a').filter(function (i, el) {
+      var $el = $(el),
+        shouldColorize = $el.find('img').length
+          || $el.closest('.tags > li, .social-media-list').length;
+      if (shouldColorize) {
+        el.className += ' no-underline';
+        return false;
+      }
+      return true;
+    }).each(function (i, el) {
+      el.className = 'color-' + i % 4 + ' ' + el.className;
+    });
+  }
+
+  function toggleHash () {
+    window.location.hash = window.location.hash.replace('#', '') ? '' : 'menu';
+  }
+
+  function toggleMenuState () {
+    $body.toggleClass('show-menu', window.location.hash === '#menu');
+  }
+
+  $(colorize);
+  $(toggleMenuState);
+  $(document.body).on('click', '.blog-title', toggleHash);
+  $(window).on('hashchange', toggleMenuState);
+})();
+
